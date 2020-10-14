9f0f3f5eec44a42082268a9e71d84df7390fac41
mr-cade <cadelpetersen@gmail.com>
2020-10-14 15:04:03 -0600

    professional experience

--- src/pages/About.js
+++ src/pages/About.js
@@ -14,19 +14,26 @@ function About() {
             <Container style={{ marginTop: 30 }}>
                 <Card>
                     <Col size="md-5">
-                        <p>
-                            I was born and raised north of Salt Lake in Bountiful, UT. Growing up, I loved to learn, ski, play sports and play videogames with my friends. I was a student at Bountiful High School where I participated in student council. Following high school I attended BYU's Marriott School of Business and majored in Finance.
-                        </p>
+                        <div class="card-title">
+                            <h4>The Early Years</h4>
+                            <p class="card-text">I was born and raised north of Salt Lake in Bountiful, UT. Growing up, I loved to learn, ski, play sports and play videogames with my friends. I was a student at Bountiful High School where I participated in student council. Following high school I attended BYU's Marriott School of Business and majored in Finance.</p>
+                            <p class="card-text">Shortly after graduating from college, I adopted a puppy who has become my best pal. He's half Bernese Mountain Dog, half Poodle, all bark and no bite.</p>
+                        </div>
                     </Col>
                     <Col size="md-5">
-                        <p>
-                            Phasellus at rutrum nisl. Praesent sed massa ut ipsum bibendum porttitor. Sed
-                            malesuada molestie velit ac viverra. Quisque a ullamcorper purus. Curabitur luctus mi
-                            ac mi hendrerit semper. Nulla tincidunt accumsan lobortis. Mauris convallis sapien non
-                            nibh porta accumsan. Nunc volutpat tempus porttitor. Nunc congue dictum egestas.
-                            Aliquam blandit mi eu urna scelerisque, vitae volutpat ligula ultricies. Maecenas vel
-                            porta augue. Fusce mauris ex, dignissim et lacinia ut, tempus eget nibh.
-                        </p>
+                        <div class="card-title">
+                        <h4>Professional Experience</h4>
+                        <h6><i class="fas fa-chart-line"></i> Comcast NBC Universal</h6>
+                            <p class="card-text">Out of college, I as recruited to participate in a corporate finance rotational program through three divisions of Comcast NBC Universal, one of the world's largest telecom and media companies. This provided me with a great breadth of experience within corporate finance</p>
+                            <p class="card-text">My three rotations took me through the following roles:</p>
+                                <ul class="pl-5">
+                                    <li>Comcast West Division Revenue Analyst -- Denver, CO</li>
+                                    <li>NBC Universal First Run Syndication Financial Analysis -- New York, NY</li>
+                                    <li>Comcast Corporate Headquarters Xfinity Home Product Analysis -- Philadelphia, PA</li>
+                                </ul>
+                        <i class="fas fa-hand-holding-usd"></i><h6> sPower</h6>
+                            <p class="card-text">After my time with Comcast, I took a job in Salt Lake with a solar development company as a treasury analyst. There I was primarily responsible for cash management. We managed to a relatively low cash balance of about $20MM</p>
+                        </div>
                     </Col>
                 </Card>
             </Container>
