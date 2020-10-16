d38899e2fdda9469e30f78ece02badaee0c63725
mr-cade <cadelpetersen@gmail.com>
2020-10-16 09:24:05 -0600

    about page formatting and text content

--- src/pages/About.js
+++ src/pages/About.js
@@ -19,11 +19,11 @@ function About() {
                     </Card>
                     <Col size="md-4">
                         <div className="card bg-light">
-                            <div className=" bg-lightcard-body">
+                            <div className="card-body">
                                 <div className="card-title">
                                     <h4>Background</h4>
                                 
-                                    <p >I was born and raised north of Salt Lake in Bountiful, UT. Growing up, I loved to learn, ski, play sports and play videogames with my friends. I was a student at Bountiful High School where I participated in student council. Following high school I attended BYU's Marriott School of Business and majored in Finance.</p>
+                                    <p >I was born and raised north of Salt Lake City in Bountiful, UT. Growing up, I loved to learn, ski, play sports and play videogames with my friends. I was a student at Bountiful High School where I participated in student council. Following high school I attended BYU's Marriott School of Business and majored in Finance.</p>
                                     <p >Shortly after graduating from college, I adopted a puppy who has become my best pal. He's half Bernese Mountain Dog, half Poodle, all bark and no bite.</p>
                                 </div>
                             </div>
@@ -34,17 +34,16 @@ function About() {
                             <div className="card-body">
                                 <div className="card-title">
                                     <h4>Professional Experience</h4>
-                                </div>
-                                <div>
-                                    <h6><i className="fas fa-chart-line"></i> Comcast NBC Universal</h6>
-                                    <p >Out of college, I as recruited to participate in a corporate finance rotational program through three divisions of Comcast NBC Universal, one of the world's largest telecom and media companies. This provided me with a great breadth of experience within corporate finance</p>
+                                
+                                    <h5><i className="fas fa-chart-line"></i> Comcast NBC Universal</h5>
+                                    <p >Out of college, I as recruited to participate in a corporate finance rotational program through three divisions of Comcast NBC Universal, one of the world's largest telecom and media companies. This provided me with a great breadth of experience within corporate finance.</p>
                                     <p >My three rotations took me through the following roles:</p>
                                     <ul className="pl-5">
                                         <li>Comcast West Division Revenue Analyst -- Denver, CO</li>
                                         <li>NBC Universal First Run Syndication Financial Analysis -- New York, NY</li>
                                         <li>Comcast Corporate Headquarters Xfinity Home Product Analysis -- Philadelphia, PA</li>
                                     </ul>
-                                    <i className="fas fa-hand-holding-usd"></i><h6> sPower</h6>
+                                    <i className="fas fa-hand-holding-usd"></i><h5> sPower</h5>
                                     <p>After my time with Comcast, I took a job in Salt Lake with a solar development company as a treasury analyst. There I was primarily responsible for cash management. We managed to a relatively low cash balance of about $20MM</p>
                                 </div>
                             </div>
