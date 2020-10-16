2911aa146586c231e2435e12df3af9e26ddde262
mr-cade <cadelpetersen@gmail.com>
2020-10-16 16:04:42 -0600

    Updates

---
+++ precache-manifest.643b3c1965f61409304ba7e6d9d36daa.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "edf2443957bd28e42e78bd465daee303",
+    "url": "/cade-portfolio/index.html"
+  },
+  {
+    "revision": "5fdcc829a8ea9e7fab0a",
+    "url": "/cade-portfolio/static/css/main.94880349.chunk.css"
+  },
+  {
+    "revision": "8e8861aaf935fd31a7f3",
+    "url": "/cade-portfolio/static/js/2.2e3b3c9a.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/cade-portfolio/static/js/2.2e3b3c9a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5fdcc829a8ea9e7fab0a",
+    "url": "/cade-portfolio/static/js/main.b63a588b.chunk.js"
+  },
+  {
+    "revision": "e152cad8f54f844c57ee",
+    "url": "/cade-portfolio/static/js/runtime-main.d5fc3777.js"
+  },
+  {
+    "revision": "b86216a488325d49fb1da2784c242ee7",
+    "url": "/cade-portfolio/static/media/IMG_0055.b86216a4.jpeg"
+  },
+  {
+    "revision": "ea8e0a00ecf44a1df176b527504f818b",
+    "url": "/cade-portfolio/static/media/covid.ea8e0a00.png"
+  },
+  {
+    "revision": "a19c8274fc163edd3caed852d9368a72",
+    "url": "/cade-portfolio/static/media/day-planner.a19c8274.png"
+  },
+  {
+    "revision": "b182f6d5a6eb2557512b0caf250dda57",
+    "url": "/cade-portfolio/static/media/js-quiz.b182f6d5.png"
+  },
+  {
+    "revision": "ebcdbb4f18445e5769be4487f4089247",
+    "url": "/cade-portfolio/static/media/note-taker.ebcdbb4f.png"
+  },
+  {
+    "revision": "4d660fd8d2af2b48ea5cf8b26d50f62b",
+    "url": "/cade-portfolio/static/media/pw-gen.4d660fd8.png"
+  },
+  {
+    "revision": "9ef3a9a258a2efc63cb4ffb5387f4543",
+    "url": "/cade-portfolio/static/media/team-builder.9ef3a9a2.png"
+  }
+]);
